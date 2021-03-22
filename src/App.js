d8024f9d0538b10442cee4318a28684211156330
Tuuni <tuuni.mustonen@metropolia.fi>
2021-03-23 01:15:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import MediaTable from './components/MediaTable';
+
+const App = () => {
+  return <MediaTable />;
+};
+
+export default App;
---
+++ src/components/MediaTable.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react';
+import MediaRow from './MediaRow';
+
+const MediaTable = () => {
+  const [picArray, setPicArray] = useState([]);
+
+  useEffect(()=>{
+    const loadMedia = async () => {
+      const response = await fetch('test.json');
+      const files = await response.json();
+      console.log(files);
+      setPicArray(files);
+    };
+
+    loadMedia();
+  }, []);
+
+
+  return (
+    <table>
+      <tbody>
+        {picArray.map((item, index) => (
+          <MediaRow key={index} file={item} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MediaTable;
